9795d77ef9627d70107cb632e762ab3d0ee7ef08
jinsheng.wu2 <jinsheng.wu2@zhenai.com>
2019-04-16 10:40:31 +0800

    update demo

--- app/src/main/java/com/salton123/saltonframeworkdemo/MainActivity.java
+++ app/src/main/java/com/salton123/saltonframeworkdemo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.salton123.saltonframeworkdemo;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ScrollView;
 
 import com.salton123.saltonframeworkdemo.test.TestAty;
@@ -17,11 +20,37 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ComponentName moduleOne;
+    private ComponentName defaultComp;
+    private PackageManager mPackageManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, TestAty.class));
+        defaultComp = getComponentName();
+        mPackageManager = getPackageManager();
+        moduleOne = new ComponentName(getApplicationContext(), "com.salton123.saltonframeworkdemo.MainTwoAty");
+        disableComp(defaultComp);
+        enableComp(moduleOne);
+        // startActivity(new Intent(MainActivity.this, TestAty.class));
+    }
+
+    private void changeIcon(View view) {
+
+    }
+
+
+    private void enableComp(ComponentName name) {
+        mPackageManager.setComponentEnabledSetting(name,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    private void disableComp(ComponentName name) {
+        mPackageManager.setComponentEnabledSetting(name,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
     }
 
 
