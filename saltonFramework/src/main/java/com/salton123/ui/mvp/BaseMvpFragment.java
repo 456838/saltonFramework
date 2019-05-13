e4b96552bcbf30e6cd82853001f94e7d9d05ec7a
jinsheng.wu2 <jinsheng.wu2@zhenai.com>
2019-05-13 16:05:40 +0800

    update

---
+++ saltonFramework/src/main/java/com/salton123/ui/mvp/BaseMvpFragment.java
@@ -0,0 +1,32 @@
+package com.salton123.ui.mvp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.salton123.ui.base.BaseFragment;
+
+/**
+ * User: 巫金生(newSalton@outlook.com)
+ * Date: 2017/7/12 15:22
+ * Time: 15:22
+ * Description:
+ */
+public abstract class BaseMvpFragment<T extends BasePresenter> extends BaseFragment implements BaseView {
+
+    protected T mPresenter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPresenter.attachView(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mPresenter != null) {
+            mPresenter.detachView();
+        }
+    }
+
+}
